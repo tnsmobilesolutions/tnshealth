2a0ab35e03ecccba51611f1122b4da2bf3cd50d7
Amrutanshu <amrutanshup@gmail.com>
2022-01-06 17:54:23 +0530

    tab bar added

--- lib/allopathy/allopathy.dart
+++ lib/allopathy/allopathy.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 class Allopathy extends StatefulWidget {
   const Allopathy({Key? key}) : super(key: key);
@@ -9,22 +8,12 @@ class Allopathy extends StatefulWidget {
 }
 
 class _AllopathyState extends State<Allopathy> {
-  void _onSelectionChanged(DateRangePickerSelectionChangedArgs args) {
-    // TODO: implement your code here
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueAccent,
       appBar: AppBar(
         title: const Text('Allopathy'),
-      ),
-      body: Container(
-        child: SfDateRangePicker(
-          onSelectionChanged: _onSelectionChanged,
-          selectionMode: DateRangePickerSelectionMode.range,
-        ),
       ),
     );
   }
--- lib/screen/Dashboard.dart
+++ lib/screen/Dashboard.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:tnshealth/screen/Login_Page.dart';
-import 'package:tnshealth/screen/allopathy.dart';
-import 'package:tnshealth/screen/ayurvedic.dart';
-import 'package:tnshealth/screen/homeopathy.dart';
+import 'package:tnshealth/allopathy/allopathy.dart';
+import 'package:tnshealth/ayurvedic/ayurvedic.dart';
+import 'package:tnshealth/homeopathy/homeopathy.dart';
 
 class DashBoard extends StatefulWidget {
   @override
@@ -10,152 +10,176 @@ class DashBoard extends StatefulWidget {
 }
 
 class _DashBoardState extends State<DashBoard> {
-  final kTextStyle = TextStyle(fontSize: 40, fontWeight: FontWeight.bold);
+  final kTextStyle = const TextStyle(fontSize: 40, fontWeight: FontWeight.bold);
+  final kTabBar = const TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: Scaffold(
+        // backgroundColor: Colors.transparent,
+        drawer: Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
                 decoration: BoxDecoration(
                   color: Colors.blue,
                 ),
                 child: Text(
                   'Menu',
                   style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                )),
-            ListTile(
-              title: const Text('Item 1'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Row(
-                children: const <Widget>[
-                  Icon(Icons.logout),
-                  Text('Logout'),
-                ],
+                ),
               ),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (context) {
-                    return const LoginPage();
-                  },
-                ));
-              },
-            ),
-          ],
+              ListTile(
+                title: Row(
+                  children: const <Widget>[
+                    Icon(Icons.logout),
+                    Text('Logout'),
+                  ],
+                ),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return const LoginPage();
+                    },
+                  ));
+                },
+              ),
+            ],
+          ),
         ),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(40.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20), color: Colors.red),
-                height: 70.0,
-                width: 300.0,
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return const Allopathy();
-                        },
-                      ),
-                    );
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Center(
-                      child: Text(
-                        'Allopathy',
-                        style: kTextStyle,
-                      ),
-                    ),
-                  ),
+        appBar: AppBar(
+          title: const Text('Home'),
+          // ignore: prefer_const_constructors
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                child: Text(
+                  'Consult',
+                  style: kTabBar,
                 ),
               ),
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.greenAccent),
-                height: 70.0,
-                width: 300.0,
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return const Ayurvedic();
-                        },
-                      ),
-                    );
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Center(
-                      child: Text(
-                        'Ayurvedic',
-                        style: kTextStyle,
-                      ),
-                    ),
-                  ),
+              Tab(
+                child: Text(
+                  'Diagnosys',
+                  style: kTabBar,
                 ),
               ),
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.grey),
-                height: 70.0,
-                width: 300.0,
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return const Homeopathy();
-                        },
-                      ),
-                    );
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Center(
-                      child: Text(
-                        'Homeopathy',
-                        style: kTextStyle,
-                      ),
-                    ),
-                  ),
+              Tab(
+                child: Text(
+                  'Medicine',
+                  style: kTabBar,
                 ),
               ),
             ],
           ),
         ),
+        body: TabBarView(children: [
+          Padding(
+            padding: const EdgeInsets.all(40.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.red),
+                  height: 70.0,
+                  width: 300.0,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return const Allopathy();
+                          },
+                        ),
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Center(
+                        child: Text(
+                          'Allopathy',
+                          style: kTextStyle,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.greenAccent),
+                  height: 70.0,
+                  width: 300.0,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return const Ayurvedic();
+                          },
+                        ),
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Center(
+                        child: Text(
+                          'Ayurvedic',
+                          style: kTextStyle,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.grey),
+                  height: 70.0,
+                  width: 300.0,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return const Homeopathy();
+                          },
+                        ),
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Center(
+                        child: Text(
+                          'Homeopathy',
+                          style: kTextStyle,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Text('sagar'),
+          Text('sagar'),
+        ]),
       ),
     );
   }
