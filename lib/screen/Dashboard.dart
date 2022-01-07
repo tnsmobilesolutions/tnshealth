7cbd33953e7818d0bfae90c60f4e0d80a5366319
Amrutanshu <amrutanshup@gmail.com>
2022-01-07 11:12:20 +0530

    scrollable view

--- lib/screen/Dashboard.dart
+++ lib/screen/Dashboard.dart
@@ -179,56 +179,86 @@ class _DashBoardState extends State<DashBoard> {
               ],
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Test Name', style: kTabBar),
-                const SizedBox(height: 20),
-                const TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Test Name',
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Text('Patient Name', style: kTabBar),
-                const SizedBox(height: 20),
-                const TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Patient Name',
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Text('Patient Adress', style: kTabBar),
-                const SizedBox(height: 20),
-                const TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Patient Adress',
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Text('Mobile No.', style: kTabBar),
-                const SizedBox(height: 20),
-                IntlPhoneField(
-                  decoration: const InputDecoration(
-                    labelText: 'Phone Number',
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(),
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  // Text('Test Name', style: kTabBar),
+                  // const SizedBox(height: 20),
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Test Name',
                     ),
                   ),
-                  initialCountryCode: 'IN',
-                  onChanged: (phone) {
-                    print(phone.completeNumber);
-                  },
-                )
-              ],
+                  const SizedBox(height: 20),
+                  // Text('Patient Name', style: kTabBar),
+                  // const SizedBox(height: 20),
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Patient Name',
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text('Patient Adress', style: kTabBar),
+                  const SizedBox(height: 20),
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Adress Line 1',
+                    ),
+                  ),
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Adress Line 2',
+                    ),
+                  ),
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'City',
+                    ),
+                  ),
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'State',
+                    ),
+                  ),
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'PinCode',
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  // Text('Mobile No.', style: kTabBar),
+                  // const SizedBox(height: 20),
+                  IntlPhoneField(
+                    decoration: const InputDecoration(
+                      labelText: 'Phone Number',
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(),
+                      ),
+                    ),
+                    initialCountryCode: 'IN',
+                    onChanged: (phone) {
+                      print(phone.completeNumber);
+                    },
+                  )
+                ],
+              ),
             ),
           ),
           Padding(
